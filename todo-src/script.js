8a8da3b22ea74510e18a6097036d08967b0beab3
Nuchaz <nuchaz.ipod@gmail.com>
2017-01-23 06:51:07 -0500

    Created the edit feature
    Created a new branch to later be merged. This branch adds an edit button on each item that can be used to edit the item. The button is functional, but issues may arise from trying to edit more than 1 entry at a time.

--- todo-src/script.js
+++ todo-src/script.js
@@ -4,12 +4,16 @@ var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
   $scope.todos = ["Learn Angular", "Learn node"];
+  $scope.editing = [false, false];
+  $scope.editTextBox = "";
   $scope.newItem = "";
+
   
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
       $scope.todos.push($scope.newItem);
+      $scope.edits.push(false);
       $scope.newItem = "";
     }
   }
@@ -18,9 +22,31 @@ myApp.controller('MainCtrl', function ($scope){
     console.log("in delete");
     var index = $scope.todos.indexOf(item);
     $scope.todos.splice(index, 1);
+    $scope.edits.splice(index, 1);
   }
-    
-  
+
+  $scope.editItem = function(item){
+    console.log("in edit");
+    var index = $scope.todos.indexOf(item);
+    var isEditingElsewhere = false;
+    for (i = 0; i < $scope.editing.length; i++){
+      if (i !== index && $scope.editing[i] == true){
+        isEditingElsewhere = true;
+      }
+    }
+    //if editing is true somewhere other than index, do nothing, user should only be able to edit 1 item at a time
+    //this doesn't stop the text / edit text field from hiding and showing, but it stops the function from breaking
+    //This could be improved, but is basically functional
+    if (!isEditingElsewhere){
+        if ($scope.editing[index]){
+            $scope.todos[index] = $scope.editTextBox;
+        } else {
+            $scope.editTextBox = $scope.todos[index];
+        }
+        $scope.editing[index] = !$scope.editing[index];
+    }
+  }
+
 });
 
 /*************************
@@ -32,5 +58,5 @@ myApp.controller('MainCtrl', function ($scope){
  * - make it prettier
  * - add a due date
  * - add reminder (setInterval)
- * 
+ *
  * *********************/
